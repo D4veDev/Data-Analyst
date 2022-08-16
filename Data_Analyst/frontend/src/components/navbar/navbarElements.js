099243dc4ba0082c25bee3ce2d0ae35d9d21ce57
D4veDev <mail.davedev@gmail.com>
2022-08-16 13:44:40 +0200

    dependency update

--- Data_Analyst/frontend/src/components/navbar/navbarElements.js
+++ Data_Analyst/frontend/src/components/navbar/navbarElements.js
@@ -8,82 +8,82 @@ import classes from "./navbarElements.module.scss";
 import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
-  const history = useNavigate();
-  const [menuOpen, setMenuOpen] = useState(false);
-  const [size, setSize] = useState({
-    width: undefined,
-    height: undefined,
-  });
+    const history = useNavigate();
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [size, setSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
 
-  useEffect(() => {
-    const handleResize = () => {
-      setSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
+    useEffect(() => {
+        const handleResize = () => {
+            setSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        };
+        window.addEventListener("resize", handleResize);
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    useEffect(() => {
+        if (size.width > 840 && menuOpen) {
+            setMenuOpen(false);
+        }
+    }, [size.width, menuOpen]);
+
+    const menuToggleHandler = () => {
+        setMenuOpen((p) => !p);
     };
-    window.addEventListener("resize", handleResize);
 
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+    const ctaClickHandler = () => {
+        menuToggleHandler();
+        history.push("/page-cta");
+    };
 
-  useEffect(() => {
-    if (size.width > 840 && menuOpen) {
-      setMenuOpen(false);
-    }
-  }, [size.width, menuOpen]);
-
-  const menuToggleHandler = () => {
-    setMenuOpen((p) => !p);
-  };
-
-  const ctaClickHandler = () => {
-    menuToggleHandler();
-    history.push("/page-cta");
-  };
-
-  return (
-    <header className={classes.navheader}>
-      <div className={classes.navheader__content}>
-        <Link to="/" className={classes.navheader__content__logo}>
-          <img src={Logo} alt="logowtf" />
-          {/* Brand Name */}
-          <h1>Data Analyst</h1>
-        </Link>
-        <nav
-          className={`${classes.navheader__content__nav} ${
-            menuOpen && size.width < 840 ? classes.isMenu : ""
-          }`}
-        >
-          <ul>
-            <li>
-              <Link to="/page-one" onClick={menuToggleHandler}>
-                Dashboard
-              </Link>
-            </li>
-            <li>
-              <Link to="/page-two" onClick={menuToggleHandler}>
-                Contribute
-              </Link>
-            </li>
-            <li>
-              <Link to="/page-three" onClick={menuToggleHandler}>
-                About
-              </Link>
-            </li>
-          </ul>
-          <button onClick={ctaClickHandler}>Login/Signup</button>
-        </nav>
-        <div className={classes.navheader__content__toggle}>
-          {!menuOpen ? (
-            <BiMenuAltRight onClick={menuToggleHandler} />
-          ) : (
-            <AiOutlineClose onClick={menuToggleHandler} />
-          )}
-        </div>
-      </div>
-    </header>
-  );
+    return (
+        <header className={classes.navheader}>
+            <div className={classes.navheader__content}>
+                <Link to="/" className={classes.navheader__content__logo}>
+                    <img src={Logo} alt="logowtf" />
+                    {/* Brand Name */}
+                    <h1>Data Analyst</h1>
+                </Link>
+                <nav
+                    className={`${classes.navheader__content__nav} ${
+                        menuOpen && size.width < 840 ? classes.isMenu : ""
+                    }`}
+                >
+                    <ul>
+                        <li>
+                            <Link to="/page-one" onClick={menuToggleHandler}>
+                                Dashboard
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/page-two" onClick={menuToggleHandler}>
+                                Contribute
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/page-three" onClick={menuToggleHandler}>
+                                About
+                            </Link>
+                        </li>
+                    </ul>
+                    <button onClick={ctaClickHandler}>Login/Signup</button>
+                </nav>
+                <div className={classes.navheader__content__toggle}>
+                    {!menuOpen ? (
+                        <BiMenuAltRight onClick={menuToggleHandler} />
+                    ) : (
+                        <AiOutlineClose onClick={menuToggleHandler} />
+                    )}
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
